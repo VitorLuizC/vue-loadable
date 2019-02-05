2463f4b68470adde3118950f6c7f0c4a27d4e67c
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-02-05 20:25:16 -0200

    :white_check_mark: create Loadable method tests

--- test/index.ts
+++ test/index.ts
@@ -35,7 +35,7 @@ test('API: it named export loadable decorator', (context) => {
   context.is(typeof loadable, 'function');
 });
 
-// ..:: Install tests ::..
+// ..:: Installation tests ::..
 
 test('Install: Vue components can extend LoadableMixin', (context) => {
   const SignInForm = Vue.component('SignInForm', {
@@ -71,4 +71,100 @@ test('Install: Loadable usage on Vue install globally (on every components)', (c
 
   context.truthy(state.LOADING_STATES);
   context.is(typeof state.LOADING_STATES, 'object');
+});
+
+// ..:: Loadable method tests ::..
+
+test('Loadable: $isLoading and $isLoadingAny returns false when no state is loading', (context) => {
+  const SignInForm = new (
+    LoadableMixin.extend({})
+  );
+
+  context.false(SignInForm.$isLoading('state'));
+  context.false(SignInForm.$isLoading());
+  context.false(SignInForm.$isLoadingAny());
+});
+
+const SignInForm = new (
+  LoadableMixin.extend({
+    methods: {
+      setSignInLoading () {
+        this.$setLoading(); // Unknown as default state.
+        this.$setLoading('signIn');
+      },
+      unsetSignInLoading () {
+        this.$unsetLoading(); // Unknown as default state.
+        this.$unsetLoading('signIn');
+      },
+    }
+  })
+);
+
+test('Loadable: $setLoading set state as loading', (context) => {
+  context.false(SignInForm.$isLoading('signIn'));
+  context.false(SignInForm.$isLoading()); // Unknown as default state.
+  context.false(SignInForm.$isLoadingAny());
+
+  SignInForm.setSignInLoading();
+
+  context.true(SignInForm.$isLoading('signIn'));
+  context.true(SignInForm.$isLoading()); // Unknown as default state.
+  context.true(SignInForm.$isLoadingAny());
+
+  SignInForm.unsetSignInLoading();
+});
+
+test('Loadable: $unsetLoading unset state as loading', (context) => {
+  SignInForm.setSignInLoading();
+
+  context.true(SignInForm.$isLoading('signIn'));
+  context.true(SignInForm.$isLoading()); // Unknown as default state.
+  context.true(SignInForm.$isLoadingAny());
+
+  SignInForm.unsetSignInLoading();
+
+  context.false(SignInForm.$isLoading('signIn'));
+  context.false(SignInForm.$isLoading()); // Unknown as default state.
+  context.false(SignInForm.$isLoadingAny());
+});
+
+test('Loadable: $isLoadingAny checks if any state is loading', (context) => {
+  const SignInForm = new (
+    LoadableMixin.extend({
+      methods: {
+        x () {
+          this.$setLoading('x');
+        },
+        y () {
+          this.$unsetLoading('x');
+          this.$setLoading('y');
+        },
+        z () {
+          this.$unsetLoading('y');
+        }
+      }
+    })
+  );
+
+  context.false(SignInForm.$isLoading('x'));
+  context.false(SignInForm.$isLoading('y'));
+  context.false(SignInForm.$isLoadingAny());
+
+  SignInForm.x();
+
+  context.true(SignInForm.$isLoading('x'));
+  context.false(SignInForm.$isLoading('y'));
+  context.true(SignInForm.$isLoadingAny());
+
+  SignInForm.y();
+
+  context.false(SignInForm.$isLoading('x'));
+  context.true(SignInForm.$isLoading('y'));
+  context.true(SignInForm.$isLoadingAny());
+
+  SignInForm.z();
+
+  context.false(SignInForm.$isLoading('x'));
+  context.false(SignInForm.$isLoading('y'));
+  context.false(SignInForm.$isLoadingAny());
 });
